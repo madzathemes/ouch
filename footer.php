d7ea94ec1826dff057d67808a27670ebebcbf39c
madzathemes <madza@madzathemes.com>
2017-10-09 21:22:27 +0300

    fix

--- footer.php
+++ footer.php
@@ -38,7 +38,7 @@
 	</div>
 </div>
 
-<a href="#" class="footer-scroll-to-top mt-theme-background"></a>
+<a href="#" class="footer-scroll-to-top mt-theme-background mt-radius"></a>
 <?php  wp_footer(); ?>
 </body>
 </html>
